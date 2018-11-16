25e390323e6e115da1e6a45ab149bc80d1a762d4
Karim <jaouane.karim@gmail.com>
2018-11-16 16:59:27 +0100

    sign up validator corrigé

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 

1573b258bd76b8faf6747b8f671bfde8b4176024
Karim <jaouane.karim@gmail.com>
2018-11-17 16:24:36 +0100

    bok list comonent

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Router } from '@angular/router';
+import { BooksService } from '../services/books.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-book-list',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
+
+  constructor(private booksService: BooksService, private router: Router) {}
 
   ngOnInit() {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
+  }
+
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+  
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
   }
 
 }
